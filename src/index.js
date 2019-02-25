41ba3a29f78134252c700902c872fd6fd32c1dd8
Your Name <dicoycv@gmail.com>
2019-02-25 01:58:28 -0300

    5th?

--- src/index.js
+++ src/index.js
@@ -17,7 +17,6 @@ const datos = {
   },
   recibos:{
     agregar:({codigo, conceptos}) => {
-      console
       datos.agregar('recibos', datos.recibos.concat({codigo, conceptos}))
     }
   },
@@ -25,6 +24,27 @@ const datos = {
     datos.agregar(nombre, valor);
   }
 };
+
+const copyStyles = (sourceDoc, targetDoc) => {
+  Array.from(sourceDoc.styleSheets).forEach(styleSheet => {
+    if (styleSheet.cssRules) { // true for inline styles
+      const newStyleEl = sourceDoc.createElement('style');
+
+      Array.from(styleSheet.cssRules).forEach(cssRule => {
+        newStyleEl.appendChild(sourceDoc.createTextNode(cssRule.cssText));
+      });
+
+      targetDoc.head.appendChild(newStyleEl);
+    } else if (styleSheet.href) { // true for stylesheets loaded from a URL
+      const newLinkEl = sourceDoc.createElement('link');
+
+      newLinkEl.rel = 'stylesheet';
+      newLinkEl.href = styleSheet.href;
+      targetDoc.head.appendChild(newLinkEl);
+    }
+  });
+}
+
 const uid = (() => {
   let i = 0;
   return () => {
@@ -64,18 +84,282 @@ datos.agregar("conceptos", [
   {nombre:"Extraordinaria", precio: 100},
   {nombre:"Donaciones", precio: 100}
 ]);
-datos.agregar("alumnos", [
-  {nombre:"Bianca Paulina Mandaradoni Tobar", codigo: "vm18", hermanos: ["am14"]},
-  {nombre:"Giulia Paulina Mandaradoni Tobar", codigo: "am12", hermanos: ["vm18"]},
-  {nombre:"Pepita Pistoletas", codigo: "ct23"},
-  {nombre:"Pepita Menganetas", codigo: "ct23"}
-]);
 datos.agregar("salas", [
-  {color: "s", codigo: "Rosa"},
-  {color: "a", codigo: "Amarilla"},
-  {color: "c", codigo: "Celeste"},
-  {color: "j", codigo: "Roja"},
-  {color: "v", codigo: "Verde"}
+  {codigo: "s", color: "Rosa"},
+  {codigo: "a", color: "Amarilla"},
+  {codigo: "c", color: "Celeste"},
+  {codigo: "j", color: "Roja"},
+  {codigo: "v", color: "Verde"}
+]);
+datos.agregar("alumnos", [
+  {nombre:'ANTONIA ARNESTO', codigo: 'st2'},
+  {nombre:'MILAGROS BELLO', codigo: 'st3'},
+  {nombre:'DELFINA BRANDOLINI', codigo: 'st4'},
+  {nombre:'GIULIANA B. COLMAN PELAYTAY', codigo: 'st5'},
+  {nombre:'JUANA GARCIA NAVARRO', codigo: 'st6'},
+  {nombre:'MILENA HASSAN BRINGIOTTI', codigo: 'st7'},
+  {nombre:'LUPE LEZCANO RANDAZZO', codigo: 'st8'},
+  {nombre:'NOELIA LOPEZ LARA', codigo: 'st9'},
+  {nombre:'MILAGROS MORETTI PERALTA', codigo: 'st10'},
+  {nombre:'ITHIEL PARISI', codigo: 'st11'},
+  {nombre:'CATALINA SERENO', codigo: 'st12'},
+  {nombre:'THIAGO AVALOS', codigo: 'st13'},
+  {nombre:'LUCIANO BASILE MALDONADO', codigo: 'st14'},
+  {nombre:'LEON BENJAMIN DUARTE ', codigo: 'st15'},
+  {nombre:'FELIPE ILLAILI', codigo: 'st16'},
+  {nombre:'ALEJO JALAF FERNANDEZ', codigo: 'st17'},
+  {nombre:'FABRIZIO NAVARRO TASSO', codigo: 'st18'},
+  {nombre:'AARON DIEGO COLIGUANTE', codigo: 'sm1'},
+  {nombre:'VALENTIN DE LA SERNA', codigo: 'sm2'},
+  {nombre:'VICENTE ENCINA ROLDAN', codigo: 'sm3'},
+  {nombre:'OLIVER JUAREZ', codigo: 'sm4'},
+  {nombre:'SEBASTIAN POLEGZUK', codigo: 'sm5'},
+  {nombre:'NICOLAS TORRES', codigo: 'sm6'},
+  {nombre:'SOFIA ABDALA LASSA', codigo: 'sm7'},
+  {nombre:'GUADALUPE ALGAMIZ', codigo: 'sm8'},
+  {nombre:'MALENA JAZMIN BAEZ', codigo: 'sm9'},
+  {nombre:'AILIN BALOR DALISSANDRO', codigo: 'sm10'},
+  {nombre:'MARTINA BERON', codigo: 'sm11'},
+  {nombre:'CLARITA CRISTALDO', codigo: 'sm12'},
+  {nombre:'JOAQUINA DOMINGUEZ OLIV.', codigo: 'sm13'},
+  {nombre:'JULIANA FERNANDEZ IP.', codigo: 'sm14'},
+  {nombre:'MARTINA LOPEZ PAREDES', codigo: 'sm15'},
+  {nombre:'EMILIA MAMPRIN', codigo: 'sm16'},
+  {nombre:'CONSTANZA PAZ MERLO RUOFF', codigo: 'sm17'},
+  {nombre:'OLIVIA SORRENTI', codigo: 'sm18'},
+
+  {nombre:'ANGEL AKIKE', codigo: 'am1'},
+  {nombre:'FABRIZIO ARIZA', codigo: 'am2'},
+  {nombre:'BASTIAN AYALA', codigo: 'am3'},
+  {nombre:'BAUTISTA CASCO', codigo: 'am4'},
+  {nombre:'FIDEL ENRIQUE FERNANDEZ', codigo: 'am5'},
+  {nombre:'JANO MAMPRIN', codigo: 'am6'},
+  {nombre:'AITHAN ANDRES MACHUCA', codigo: 'am7'},
+  {nombre:'BAUTISTA MARSILI MORE', codigo: 'am8'},
+  {nombre:'CIRO MENDOZA', codigo: 'am9'},
+  {nombre:'FELIPE SIMON MERLO RUOFF', codigo: 'am10'},
+  {nombre:'VALENTINO MORETTA', codigo: 'am11'},
+  {nombre:'LAUTARO PEREZ', codigo: 'am12'},
+  {nombre:'SANTINO SIMON QUINTANS', codigo: 'am13'},
+  {nombre:'DANTE SEPPIA', codigo: 'am14'},
+  {nombre:'DANTE MANUEL VERA', codigo: 'am15'},
+  {nombre:'FELIPE VIGGIANO', codigo: 'am16'},
+  {nombre:'MALENA FRANCO', codigo: 'am17'},
+  {nombre:'MARIA GUADALUPE ALBORNOZ', codigo: 'am18'},
+  {nombre:'OLIVIA BARRANQUERO', codigo: 'am19'},
+  {nombre:'GUILLERMINA GONZALEZ ', codigo: 'am20'},
+  {nombre:'ALMA HIDALGO', codigo: 'am21'},
+  {nombre:' MANDARADONI TOBAR GIULIA', codigo: 'am22'},
+  {nombre:'AMBAR JULIETA MORE', codigo: 'am23'},
+  {nombre:'LIBERTAD MORENO RICHIERI', codigo: 'am24'},
+  {nombre:'ANA VALENTINA RODRIGUEZ ', codigo: 'am25'},
+  {nombre:'CONSTANZA MARINA SOSA', codigo: 'am26'},
+  {nombre:'AMBAR FRANCESCA SOSA SANCHEZ', codigo: 'am27'},
+  {nombre:'NARELA YUNES FRANCO', codigo: 'am28'},
+  {nombre:'ISABELA ESPOSITO', codigo: 'am29'},
+  {nombre:'BAUTISTA DOMINGO ALI', codigo: 'at1'},
+  {nombre:'MATEO JOSE AMARILLO', codigo: 'at2'},
+  {nombre:'ELISEO BALZAROTTI', codigo: 'at3'},
+  {nombre:'SIMON BIGGIOTTI', codigo: 'at4'},
+  {nombre:'IGNACIO BUSQUETS STRAUBS', codigo: 'at5'},
+  {nombre:'AGUSTIN CARRION', codigo: 'at6'},
+  {nombre:'IGNACIO CURRA', codigo: 'at7'},
+  {nombre:'LEON FERNANDEZ CASCO', codigo: 'at8'},
+  {nombre:'TOMAS GIMENES', codigo: 'at9'},
+  {nombre:'ROMEO LEON GONZALEZ ', codigo: 'at10'},
+  {nombre:'PEDRO IGNACIO JONES', codigo: 'at11'},
+  {nombre:'IAN DEMIR LEDESMA', codigo: 'at12'},
+  {nombre:'VALENTIN MASSIMINO SOLOMITA', codigo: 'at13'},
+  {nombre:'FRANCISCO PIEDRABUENA PEREZ', codigo: 'at14'},
+  {nombre:'PEDRO SANCHEZ', codigo: 'at15'},
+  {nombre:'FRANCISCO SANCHEZ', codigo: 'at16'},
+  {nombre:'FLORENCIA CAPELLETTI', codigo: 'at17'},
+  {nombre:'DULCE MARIA DE BONO SEGUNDO', codigo: 'at18'},
+  {nombre:'SOL NAIARA DUARTE JAIMES', codigo: 'at19'},
+  {nombre:'AMANDA GAJARDO DOMINGUEZ', codigo: 'at20'},
+  {nombre:'JULIETA GONZALEZ ACEVEDO', codigo: 'at21'},
+  {nombre:'JULIA INES LABORDE ', codigo: 'at22'},
+  {nombre:'CATALINA MILAGROS LICITRA', codigo: 'at23'},
+  {nombre:'CATALINA LOPEZ', codigo: 'at24'},
+  {nombre:'AZUL VICTORIA MACCHI', codigo: 'at25'},
+  {nombre:'BRISA MILAGROS MANSILLA', codigo: 'at26'},
+  {nombre:'DELFINA MERLO', codigo: 'at27'},
+  {nombre:'LIBERTAD MAURI CALOS', codigo: 'at28'},
+
+  {nombre:'VALENTIN ALGAMIZ', codigo: 'am1'},
+  {nombre:'RAMIRO AVILA LUNA', codigo: 'am2'},
+  {nombre:'GAEL BARRAZA', codigo: 'am3'},
+  {nombre:'ABRIL DEZE', codigo: 'am4'},
+  {nombre:'FELIPE FERNANDEZ IPAR', codigo: 'am5'},
+  {nombre:'BAUTISTA GALANTE', codigo: 'am6'},
+  {nombre:'JEREMIAS GALEANO', codigo: 'am7'},
+  {nombre:'VALENTIN IBARCENA', codigo: 'am8'},
+  {nombre:'BAUTISTA ILLOBRE', codigo: 'am9'},
+  {nombre:'SIMON MOJICA COPPOLA', codigo: 'am10'},
+  {nombre:'MATEO SCALISE MORE', codigo: 'am11'},
+  {nombre:'JUAN IGNACIO SCOTTO', codigo: 'am12'},
+  {nombre:'MARTIN VEGA LISAN', codigo: 'am13'},
+  {nombre:'GERONIMO VILLALBA', codigo: 'am14'},
+  {nombre:'AINHOA AMELIE ACEVEDO', codigo: 'am15'},
+  {nombre:'CATALINA AGUERO IRIBARREN', codigo: 'am16'},
+  {nombre:'LUDMILA BAEZ', codigo: 'am17'},
+  {nombre:'LOLA CASTRO COLLAZO', codigo: 'am18'},
+  {nombre:'OLIVIA DE LA SERNA', codigo: 'am19'},
+  {nombre:'ISABELLA ESPOSITO', codigo: 'am20'},
+  {nombre:'MAILEN FAUSCIANA', codigo: 'am21'},
+  {nombre:'LOLA PEREZ', codigo: 'am22'},
+  {nombre:'JAZMIN PETRAGLIA', codigo: 'am23'},
+  {nombre:'VERA RODRIGUEZ RICHIERI', codigo: 'am24'},
+  {nombre:'ANA SOFIA ROMANO SANCHEZ', codigo: 'am25'},
+  {nombre:'ISABELLA RUIZ DIAZ', codigo: 'am26'},
+  {nombre:'JUSTINA SALADINO DARQUIER', codigo: 'am27'},
+  {nombre:'ANTONIA VITELLI', codigo: 'am28'},
+  {nombre:'THIAGO ROMERO', codigo: 'am29'},
+  {nombre:'BENJAMIN AUDUBERT', codigo: 'at1'},
+  {nombre:'ROMAN FEDERICO BELLO', codigo: 'at2'},
+  {nombre:'MAXIMO AUGUSTO FERREYRA', codigo: 'at3'},
+  {nombre:'PEDRO GARCIA NAVARRO', codigo: 'at4'},
+  {nombre:'JEREMIAS SALVADOR GUZMAN', codigo: 'at5'},
+  {nombre:'LUCIANO TAHIEL JAN', codigo: 'at6'},
+  {nombre:'FRANCISCO LEON', codigo: 'at7'},
+  {nombre:'TAHIEL MEDINA', codigo: 'at8'},
+  {nombre:'LAUTARO IVAN PARDO NUÑEZ', codigo: 'at9'},
+  {nombre:'SANTINO ROJAS', codigo: 'at10'},
+  {nombre:'HECTOR JOSE ROLDAN', codigo: 'at11'},
+  {nombre:'THIAGO ROMERO', codigo: 'at12'},
+  {nombre:'MILAN SALVATIERRA', codigo: 'at13'},
+  {nombre:'LEANDRO GABRIEL TORRES', codigo: 'at14'},
+  {nombre:'JOAQUIEN BENICIO VENTUIZ', codigo: 'at15'},
+  {nombre:'TIZIANO BENJAMIN YOVINO', codigo: 'at16'},
+  {nombre:'GAIA FRANCESCA ABRAHAM', codigo: 'at17'},
+  {nombre:'LUDMILA MARIA ALOMO', codigo: 'at18'},
+  {nombre:'LUZ GERALDINE AMBROSONI', codigo: 'at19'},
+  {nombre:'LIBERTAD COSMAN VEIGA', codigo: 'at20'},
+  {nombre:'JUANA RENATA DUARTE', codigo: 'at21'},
+  {nombre:'MAIA FIGUEROA', codigo: 'at22'},
+  {nombre:'LARA ELUNEY GALAN', codigo: 'at23'},
+  {nombre:'AGOSTINA IURO', codigo: 'at24'},
+  {nombre:'NICOLE MAGALI MANSILLA', codigo: 'at25'},
+  {nombre:'TIFFANY JULIETA SALINA', codigo: 'at26'},
+  {nombre:'INDIA ELOISE SCIAN PERPEN', codigo: 'at27'},
+  {nombre:'ISABELA WHEELER ', codigo: 'at28'},
+  {nombre:'FRANCO CARETTI', codigo: 'at29'},
+
+  {nombre:'FELIPE AGUIRRE JUAREZ', codigo: 'jm1'},
+  {nombre:'JEREMIAS ARCE', codigo: 'jm2'},
+  {nombre:'LIHUEL CASCO', codigo: 'jm3'},
+  {nombre:'JOAQUIN COLIGUANTE', codigo: 'jm4'},
+  {nombre:'HENRY ISMAEL DE MORA', codigo: 'jm5'},
+  {nombre:'THIAN BENJAMIN GEREZ', codigo: 'jm6'},
+  {nombre:'BENICIO JAN MADRID', codigo: 'jm7'},
+  {nombre:'No sabemos como se llama', codigo: 'jm8'},
+  {nombre:'JOAQUIN PEREZ BENTRON', codigo: 'jm9'},
+  {nombre:'FRANCISCO RODRIGUEZ ', codigo: 'jm10'},
+  {nombre:'AUGUSTO ROMAN', codigo: 'jm11'},
+  {nombre:'ELIEL VERA MADOERY', codigo: 'jm12'},
+  {nombre:'NEREA ARGAÑARAZ', codigo: 'jm13'},
+  {nombre:'HUILEN BALOR DALISSANDRO', codigo: 'jm14'},
+  {nombre:'ANITA CRISTALDO ', codigo: 'jm15'},
+  {nombre:'MIA ABIGAIL CHIRILA', codigo: 'jm16'},
+  {nombre:'GENESIS HOYOS SANDOVAL', codigo: 'jm17'},
+  {nombre:'MIA ZAMARA IRAMAIN', codigo: 'jm18'},
+  {nombre:'ORIANA ANTONELLA LOBO', codigo: 'jm19'},
+  {nombre:'MAITE LOPEZ', codigo: 'jm20'},
+  {nombre:'LOURDES MACHUCA', codigo: 'jm21'},
+  {nombre:'UMA MORALES', codigo: 'jm22'},
+  {nombre:'MIA BELEN MORE', codigo: 'jm23'},
+  {nombre:'SOFIA VICTORIA NOBLE', codigo: 'jm24'},
+  {nombre:'MARTINA AZUL ORONA', codigo: 'jm25'},
+  {nombre:'OLIVIA ELBA PELLETIERI', codigo: 'jm26'},
+  {nombre:'PALOMA RAMIREZ FARIAS', codigo: 'jm27'},
+  {nombre:'LUDMILA VIDELA', codigo: 'jm28'},
+  {nombre:'ABRIL ORIANA DEZE', codigo: 'jm29'},
+  {nombre:'AGUSTIN ADDATI BRINGIOTTI', codigo: 'jt1'},
+  {nombre:'BASTIAN ALEJO ARTAZA', codigo: 'jt2'},
+  {nombre:'MATEO BELLO', codigo: 'jt3'},
+  {nombre:'FRANCO ENRIQUE FERNANDEZ', codigo: 'jt4'},
+  {nombre:'JOAQUIN GAUCHEZ', codigo: 'jt5'},
+  {nombre:'DIEGO ETHAN HIDALGO AYALA', codigo: 'jt6'},
+  {nombre:'JUAN MANUEL LOPEZ', codigo: 'jt7'},
+  {nombre:'TOMAS NAHUEL MIRANDA', codigo: 'jt8'},
+  {nombre:'FELIPE PAZ', codigo: 'jt9'},
+  {nombre:'DANTE FEDERICO RIEDEL', codigo: 'jt10'},
+  {nombre:'CIRO ROJAS', codigo: 'jt11'},
+  {nombre:'TEO BENJAMIN SANCHEZ', codigo: 'jt12'},
+  {nombre:'LUIS ANDRES VILLARROEL CORASPE', codigo: 'jt13'},
+  {nombre:'SANTIAGO VIOLO', codigo: 'jt14'},
+  {nombre:'AXEL URRUTIA', codigo: 'jt15'},
+  {nombre:'PILAR CARRIZO VAZQUEZ', codigo: 'jt16'},
+  {nombre:'ALMA COLMAN PELAYTAY', codigo: 'jt17'},
+  {nombre:'SHEILA FARIAS ', codigo: 'jt18'},
+  {nombre:"FRANCESCA FERREIRA D'ANGELO", codigo: 'jt19'},
+  {nombre:'RENATA CLARA FRATI VANEGAS', codigo: 'jt20'},
+  {nombre:'ORNELLA ALMA FRATI VANEGAS', codigo: 'jt21'},
+  {nombre:'MIA PILAR HIDALGO ZEBALLOS', codigo: 'jt22'},
+  {nombre:'DELFINA LOPEZ QUAGLIATA', codigo: 'jt23'},
+  {nombre:'UMMA ABRIL MONJES', codigo: 'jt24'},
+  {nombre:'VICTORIA MOREL', codigo: 'jt25'},
+  {nombre:'EMMA SACHRITZ', codigo: 'jt26'},
+  {nombre:'KYARA SILVA', codigo: 'jt27'},
+  {nombre:'EMILCE NAHIARA YERBA ALCARAZ', codigo: 'jt28'},
+  {nombre:'ANALIA LLANO ', codigo: 'jt29'},
+
+  {nombre:'DIEGO BESTEIRO POLEDRI', codigo: 'vm1'},
+  {nombre:'ISAAC RAMIRO BRITEZ', codigo: 'vm2'},
+  {nombre:'BENJAMIN TOMAS DUARTE', codigo: 'vm3'},
+  {nombre:'RAMIRO GENTILE', codigo: 'vm4'},
+  {nombre:'ALAN GOMEZ COSTA', codigo: 'vm5'},
+  {nombre:'JUAN FRANCISCO LOPEZ MORENO', codigo: 'vm6'},
+  {nombre:'FELIPE LOPEZ PAREDES', codigo: 'vm7'},
+  {nombre:'DANTE MALDONADO OCHOA', codigo: 'vm8'},
+  {nombre:'CAMILO PERALTA CASTRO', codigo: 'vm9'},
+  {nombre:'TAHIEL SORIA', codigo: 'vm10'},
+  {nombre:'LUCIANO SOSA', codigo: 'vm11'},
+  {nombre:'FELICITAS AGUIRRE', codigo: 'vm12'},
+  {nombre:'ALMA BAEZ', codigo: 'vm13'},
+  {nombre:'MARTINA ALICE', codigo: 'vm14'},
+  {nombre:'AMBAR NEREA BAEZ', codigo: 'vm15'},
+  {nombre:'ISABELA BERON', codigo: 'vm16'},
+  {nombre:'JULIA DEL CASTILLO', codigo: 'vm17'},
+  {nombre:'BIANCA MANDARADONI TOBAR', codigo: 'vm18'},
+  {nombre:'JAZMIN PENELOPE PEREIRA', codigo: 'vm19'},
+  {nombre:'ALMA QUIROGA', codigo: 'vm20'},
+  {nombre:'LUANA RODRIGUEZ CISNEROS', codigo: 'vm21'},
+  {nombre:'ADELA ELIS THAYER', codigo: 'vm22'},
+  {nombre:'EMILIANA PIA TORRES', codigo: 'vm23'},
+  {nombre:'PENELOPE TRIVIÑO', codigo: 'vm24'},
+  {nombre:'EMMA TURK', codigo: 'vm25'},
+  {nombre:'ALMA YAMIL VALENZUELA', codigo: 'vm26'},
+  {nombre:'JULIA VITELLI', codigo: 'vm27'},
+  {nombre:'EMILIA VITELLI', codigo: 'vm28'},
+  {nombre:'BENJAMIN AVENTUROSO', codigo: 'vt1'},
+  {nombre:'GAEL CENTURION MURUJOS', codigo: 'vt2'},
+  {nombre:'BRUNO VALENTIN CRESCIULLO', codigo: 'vt3'},
+  {nombre:'GIANFRANCO GUGLIELMETTI', codigo: 'vt4'},
+  {nombre:'RENZO GUGLIELMETTI', codigo: 'vt5'},
+  {nombre:'LOGAN NOHA HERLEIN', codigo: 'vt6'},
+  {nombre:'BENJAMIN ILLAILI', codigo: 'vt7'},
+  {nombre:'NATANAEL LLAUDET', codigo: 'vt8'},
+  {nombre:'BRUNO LUNA ECHAZU', codigo: 'vt9'},
+  {nombre:'BRUNO MAXIMO PERALTA', codigo: 'vt10'},
+  {nombre:'JOAQUIN PEDRO PINTO', codigo: 'vt11'},
+  {nombre:'MATEO RODRIGUEZ', codigo: 'vt12'},
+  {nombre:'OLIVIA BASILE', codigo: 'vt13'},
+  {nombre:'CATALINA LOURDES BUSQUETS STRUBS', codigo: 'vt14'},
+  {nombre:'CATALINA BUSTAMANTE', codigo: 'vt15'},
+  {nombre:'FRANCESCA BUSTAMANTE', codigo: 'vt16'},
+  {nombre:'SOFIA CASALI', codigo: 'vt17'},
+  {nombre:'GINA BIANCA CAPELLETTI', codigo: 'vt18'},
+  {nombre:'VICTORIA GONZALEZ ACEVEDO', codigo: 'vt19'},
+  {nombre:'MALENA GUANTAY', codigo: 'vt20'},
+  {nombre:'SOFIA HOLMQUIST CARBONARO', codigo: 'vt21'},
+  {nombre:'LUCRECIA MARTINEZ', codigo: 'vt22'},
+  {nombre:'LUNA ATENEA RODRIGUEZ', codigo: 'vt23'},
+  {nombre:'VALENTINA TRIPODI', codigo: 'vt24'},
+  {nombre:'CAROLINA AYELEN ZALIEGA', codigo: 'vt25'},
+  {nombre:'ALMA BELEN ZARATE HEE', codigo: 'vt26'},
+  {nombre:'ALEX ALEGRE', codigo: 'vt27'},
+  {nombre:'BENICIO MAS', codigo: 'vt28'},
 ]);
 //contextos
 const Gc = React.createContext();
@@ -83,10 +367,11 @@ class Gcc extends React.Component {
 
   constructor(props) { 
     super(props);
+    const now = new Date();
     this.state = {
       alumno: {},
       nombre: "",
-      items: [{concepto: "Cuota Social", mes: "Mar", precio: datos.conceptos.filter(c=>c.nombre=="Cuota Social")[0].precio}]
+      items: [{concepto: "Cuota Social", mes: datos.meses[now.getMonth()], precio: datos.conceptos.filter(c=>c.nombre=="Cuota Social")[0].precio}]
     };
   }
 
@@ -212,7 +497,7 @@ const Autocomplete = ({css, nombre, changers, suggestions, value}) => {
       { context => (
         <div className="flex flex-column w-30">
           <Campo css={css} nombre={nombre} value={value} onChange={e=>changers.nombre(e.target.value)}  />
-          <div className={`flex flex-column bg-white br3 ${css}`}>
+          <div className={`flex flex-column bg-white br3 pr3 ${css}`}>
             {
               context.state.nombre && suggestions
                 .filter(s => s.nombre.toLowerCase().indexOf(context.state.nombre.toLowerCase())!=-1 && context.state.nombre.toLowerCase() != s.nombre.toLowerCase())
@@ -308,7 +593,7 @@ const NuevoRecibo = props => {
                 <Autocomplete css="gold" nombre="Nombre" value={context.state.nombre} changers={context.changers} suggestions={datos.alumnos} />
                 {context.state.alumno.nombre && <label className="fw9 f4">
                   {`${context.state.alumno.nombre
-                  } - Sala ${datos.salas.filter(s=>context.state.alumno.codigo[0]==s.codigo).color
+                  } - Sala ${datos.salas.filter(s => context.state.alumno.codigo[0]==s.codigo)[0].color
                   } - Turno ${context.state.alumno.codigo[1]=="m"?"Mañana":"Tarde"}`}
                 </label>}
               </div>
@@ -536,7 +821,7 @@ const Alumnos = props => {
       <div className="w-100 pb1 bb b--white-50  inline-flex items-center justify-between">
         <div className="ttu f6 fw2">Alumnos</div>
       </div>
-      <div className="pt3 f2 f2-m fw5 w-100 inline-flex items-center">256</div>
+      <div className="pt3 f2 f2-m fw5 w-100 inline-flex items-center">{datos.alumnos.length}</div>
       <div className="pt2 w-100 inline-flex items-center justify-between">
         <a className="link dim white ttu f6 fw6" href="#" title="Contact">
           Reporte
